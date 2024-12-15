01aa7312f7faa396484cbef75bd5907b3ae6068d
Shu11336 <moazshouman@gmail.com>
2024-12-15 21:58:41 +0200

    starting

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ecommerce project.
+URL configuration for myproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('products.urls')), # include the products URLconf for the 'products' app
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
